5331d525907b48db2ac27c1eaab281f5e2c90775
Rovna99 <tjdalswodi91@gmail.com>
2021-04-13 16:38:46 +0900

    +onclick
    later..

--- js/todo.js
+++ js/todo.js
@@ -1,105 +1,74 @@
-const pendingList = document.getElementById("js-pending"),
-  formTodo = document.getElementById("js-form__todo"),
-  inputTodo = formTodo.querySelector("input");
+const toDoForm = document.querySelector(".js-toDoForm"),
+  toDoInput = toDoForm.querySelector("input"),
+  toDoList = document.querySelector(".js-toDoList");
 
-const PENDING = "PENDING";
+const TODOS_LS = "toDos";
 
-let pendingTasks;
+let toDos = [];
 
-function getTaskObject(text) {
-  return {
-    id: String(Date.now()),
-    text,
-  };
-}
-
-function savePendingTask(task) {
-  pendingTasks.push(task);
-}
-
-function findInPending(taskId) {
-  return pendingTasks.find(function (task) {
-    return task.id === taskId;
+function deleteToDo(event) {
+  const btn = event.target;
+  const li = btn.parentNode;
+  toDoList.removeChild(li);
+  const cleanToDos = toDos.filter(function (toDo) {
+    return toDo.id !== parseInt(li.id);
   });
+  toDos = cleanToDos;
+  saveToDos();
 }
 
-function removeFromPending(taskId) {
-  pendingTasks = pendingTasks.filter(function (task) {
-    return task.id !== taskId;
-  });
+function saveToDos() {
+  localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
 }
 
-function addToPending(task) {
-  pendingTasks.push(task);
-}
-
-function deleteTask(e) {
-  const li = e.target.parentNode;
-  li.parentNode.removeChild(li);
-  removeFromPending(li.id);
-  saveState();
-}
-
-function handleFinishClick(e) {
-  const li = e.target.parentNode;
-  li.parentNode.removeChild(li);
-  const task = findInPending(li.id);
-  removeFromPending(li.id);
-
-  saveState();
-}
-
-function buildGenericLi(task) {
+function paintToDo(text) {
   const li = document.createElement("li");
+  const delBtn = document.createElement("button");
   const span = document.createElement("span");
-  const deleteBtn = document.createElement("button");
-  li.setAttribute("class", "todo-margin");
-  deleteBtn.setAttribute("class", "btnTodo");
-  span.innerText = task.text;
-  deleteBtn.innerText = "✔";
-  span.style.wordBreak = "break-word";
-  deleteBtn.addEventListener("click", deleteTask);
-  li.append(span, deleteBtn);
-  li.id = task.id;
-  return li;
-}
-
-function paintPendingTask(task) {
-  const genericLi = buildGenericLi(task);
-  const completeBtn = document.createElement("button");
-  completeBtn.setAttribute("class", "btnTodo");
-  completeBtn.innerText = "✖";
-  completeBtn.addEventListener("click", handleFinishClick);
-  pendingList.append(genericLi);
-  genericLi.append(completeBtn);
-}
-
-function saveState() {
-  localStorage.setItem(PENDING, JSON.stringify(pendingTasks));
-}
-
-function loadState() {
-  pendingTasks = JSON.parse(localStorage.getItem(PENDING)) || [];
-}
-
-function restoreState() {
-  pendingTasks.forEach(function (task) {
-    paintPendingTask(task);
+  const finishBtn = document.createElement("button");
+  const newId = toDos.length + 1;
+  const spanId = toDos.length + 1;
+  delBtn.innerText = "✖";
+  delBtn.addEventListener("click", deleteToDo);
+  finishBtn.innerText = "✔";
+  finishBtn.addEventListener("click", () => {
+    span.classList.toggle("Tdeco");
   });
+  span.innerText = text;
+  li.appendChild(span);
+  li.appendChild(finishBtn);
+  li.appendChild(delBtn);
+  li.id = newId;
+  span.classList.add(spanId);
+  toDoList.appendChild(li);
+  const toDoObj = {
+    text: text,
+    id: newId,
+  };
+  toDos.push(toDoObj);
+  saveToDos();
 }
 
-function handleFormSubmit(e) {
-  e.preventDefault();
-  const taskObj = getTaskObject(inputTodo.value);
-  inputTodo.value = "";
-  paintPendingTask(taskObj);
-  savePendingTask(taskObj);
-  saveState();
+function handleSubmit(event) {
+  event.preventDefault();
+  const currentValue = toDoInput.value;
+  paintToDo(currentValue);
+  toDoInput.value = "";
+}
+
+function loadToDos() {
+  const loadedToDos = localStorage.getItem(TODOS_LS);
+  if (loadedToDos !== null) {
+    const parsedToDos = JSON.parse(loadedToDos);
+    parsedToDos.forEach(function (toDo) {
+      paintToDo(toDo.text);
+    });
+  }
 }
 
 function init() {
-  formTodo.addEventListener("submit", handleFormSubmit);
-  loadState();
-  restoreState();
+  loadToDos();
+  toDoForm.addEventListener("submit", handleSubmit);
 }
+
 init();
