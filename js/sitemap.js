997a2bf11ab3b99986d9d42c4c828316dfb64551
Rovna99 <tjdalswodi91@gmail.com>
2021-05-01 10:44:39 +0900

    site

--- js/sitemap.js
+++ js/sitemap.js
@@ -1,1 +1,9 @@
-const siteMap = document.querySelector(".bottom-sitebar");
+const siteMap = document.querySelector(".sitebar-site"),
+  sitebubble1 = document.querySelector(".sitebar-bubble1"),
+  sitebubble2 = document.querySelector(".sitebar-bubble2");
+
+function showSite(event) {
+  siteMap.classList.toggle("show_sitebar");
+  sitebubble1.classList.toggle("show_sitebar");
+  sitebubble2.classList.toggle("show_sitebar");
+}
